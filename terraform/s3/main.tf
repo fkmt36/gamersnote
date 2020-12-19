a7690ff8eec74cbe6565b91484de6edec0f836a7
Ryota Fukumoto <happyfukumoto@gmail.com>
2020-12-19 22:25:24 +0900

    add s3 domain alias

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -37,7 +37,8 @@ resource "aws_route53_record" "main" {
   type    = "A"
 
   alias {
-    name    = aws_s3_bucket.main.website_domain
-    zone_id = aws_s3_bucket.main.hosted_zone_id
+    name                   = aws_s3_bucket.main.website_domain
+    zone_id                = aws_s3_bucket.main.hosted_zone_id
+    evaluate_target_health = true
   }
 }
