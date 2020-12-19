2fdd474816bf58ae2aac4aa7d5f6ef22ee6cd0fa
Ryota Fukumoto <happyfukumoto@gmail.com>
2020-12-19 20:42:30 +0900

    add rds terraform

---
+++ terraform/rds/main.tf
@@ -0,0 +1,17 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgresql"
+  engine_version       = "12.4"
+  instance_class       = "db.t2.micro"
+  name                 = "gamersnote-db"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.postgres12"
+  db_subnet_group_name = aws_db_subnet_group.main
+}
