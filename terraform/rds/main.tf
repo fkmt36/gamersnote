3c49084c8906ab1bf41ea14efb24d85a065bbd30
Ryota Fukumoto <happyfukumoto@gmail.com>
2020-12-20 01:15:10 +0900

    fix rds

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -3,19 +3,19 @@
 #   subnet_ids = var.public_subnets
 # }
 
-# resource "aws_db_instance" "main" {
-#   identifier                = "${var.name}-db"
-#   allocated_storage         = 20
-#   storage_type              = "gp2"
-#   engine                    = "postgres"
-#   engine_version            = "12.4"
-#   instance_class            = "db.t2.micro"
-#   name                      = var.name
-#   username                  = var.db_username
-#   password                  = var.db_password
-#   parameter_group_name      = "default.postgres12"
-#   db_subnet_group_name      = var.database_subnet_group_name
-#   skip_final_snapshot       = false
-#   final_snapshot_identifier = "${var.name}-db-snapshot"
-#   publicly_accessible       = true
-# }
+resource "aws_db_instance" "main" {
+  identifier                = "${var.name}-db"
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "postgres"
+  engine_version            = "12.4"
+  instance_class            = "db.t2.micro"
+  name                      = var.name
+  username                  = var.db_username
+  password                  = var.db_password
+  parameter_group_name      = "default.postgres12"
+  db_subnet_group_name      = var.database_subnet_group_name
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "${var.name}-db-snapshot"
+  publicly_accessible       = true
+}
