93c79b01a17dcfb7700e54c7a6ccc6165484597d
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-01-23 10:15:09 +0900

    delete s3 domain

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -27,18 +27,18 @@ resource "aws_s3_bucket_policy" "main" {
 POLICY
 }
 
-data "aws_route53_zone" "main" {
-  name = var.domain
-}
+# data "aws_route53_zone" "main" {
+#   name = var.domain
+# }
 
-resource "aws_route53_record" "main" {
-  zone_id = data.aws_route53_zone.main.id
-  name    = "img"
-  type    = "A"
+# resource "aws_route53_record" "main" {
+#   zone_id = data.aws_route53_zone.main.id
+#   name    = "img"
+#   type    = "A"
 
-  alias {
-    name                   = aws_s3_bucket.main.website_domain
-    zone_id                = aws_s3_bucket.main.hosted_zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_s3_bucket.main.website_domain
+#     zone_id                = aws_s3_bucket.main.hosted_zone_id
+#     evaluate_target_health = true
+#   }
+# }
