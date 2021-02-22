d2d29d70082ba210274ec68fc107b6cbb657e2e0
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-02-23 00:29:11 +0900

    set ip limitation

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["133.200.211.64"]
   }
 
   egress {
@@ -24,11 +24,12 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group_rule" "http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+  # cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["133.200.211.64"]
   security_group_id = aws_security_group.alb.id
 }
 
