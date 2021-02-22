a9e24c55931f56ac0a5329dee2b005add138b129
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-02-23 01:21:43 +0900

    fix terraform

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -24,11 +24,11 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group_rule" "http" {
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   # cidr_blocks       = ["133.200.211.64/32"]
   security_group_id = aws_security_group.alb.id
 }
