6e6f80cc59c4df0a721ee2b2bdc0ef01801f542a
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-02-23 19:25:42 +0900

    delete alb listener rule

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -103,23 +103,26 @@ module "aws_alb" {
     {
       https_listener_index = 0
 
-      actions = [{
-        type        = "redirect"
-        status_code = "HTTP_301"
-        host        = "gamersnote.com"
-        protocol    = "HTTPS"
-        },
-        {
-          type               = "forward"
-          target_group_index = 0
-      }]
+      actions = []
+      # actions = [{
+      #   type        = "redirect"
+      #   status_code = "HTTP_301"
+      #   host        = "gamersnote.com"
+      #   protocol    = "HTTPS"
+      #   },
+      #   {
+      #     type               = "forward"
+      #     target_group_index = 0
+      # }]
 
-      conditions = [{
-        host_header = ["54.64.220.254"]
-        },
-        {
-          path_patterns = ["/api/*"]
-      }]
+      conditions = []
+
+      # conditions = [{
+      #   host_header = ["54.64.220.254"]
+      #   },
+      #   {
+      #     path_patterns = ["/api/*"]
+      # }]
     }
   ]
 
