417bbd2c2bb25fc60812c49f6a023940d5a73a8b
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-02-24 12:36:58 +0900

    modify security group settings

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # GamersNote
 
-## CI・CD
-mainブランチへのpushやmergeが行われた際、ECRにイメージをpushし、Terraformのapplyが行われる。pullリクエストが行われた際、Terraformのfmtチェックとplanが行われる。
+ゲームに関する記事を投稿できるメディアプラットフォーム「GamersNote」を開発しました。
+
+![GamersNoteの画像](app/static/ogp.png)
+
+## GamersNoteの概要
+
+GamersNoteはゲームに関する記事を投稿できるメディアプラットフォームです。[note](https://note.com/)をゲームに特化させたら面白いのではないかと思い作りました。
+
+開発にかかった日数
+
--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -23,14 +23,14 @@ resource "aws_security_group" "alb" {
   }
 }
 
-# resource "aws_security_group_rule" "http" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.alb.id
-# }
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
 
 # ドメインの紐付け
 data "aws_route53_zone" "this" {
